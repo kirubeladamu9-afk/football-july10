ccc08bb6dbdb12435dc2adbd83fc728a02777364
kirubeladamu9-afk <kirubeladamu9@gmail.com>
2026-07-09 16:16:10 +0300

    Update _document.jsx

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -12,6 +12,11 @@ export default function Document() {
           rel="stylesheet"
           href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css"
         />
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="manifest" href="/site.webmanifest" />
       </Head>
       <body>
         <Main />
